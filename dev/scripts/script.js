e8264c82db7e58f97bc9ce5bc6794d079ed931b2
Pat Johnston <pattrashgarbage@gmail.com>
2018-01-04 16:35:43 -0500

    gravity & jump functional

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -5,13 +5,14 @@ const manPosition = {
 	x: 0,
 	y: 0,
 	xVelocity: 0,
-	yVelocity: 0
+	yVelocity: 0,
 }
 
 const speed = 10;
 const gravitySpeed = 1;
-const jumpSpeed = 5;
+const jumpSpeed = 22;
 let onGround = false;
+let lrUp = true;
 
 function gravity() {
 	if (!onGround) {
@@ -20,27 +21,29 @@ function gravity() {
 }
 
 function hitTest() {
-	console.log(man.offsetTop + man.offsetHeight, ground.offsetTop);
+	//console.log(man.offsetTop + man.offsetHeight, ground.offsetTop);
 	if (man.offsetTop + man.offsetHeight <= ground.offsetTop) {
 		onGround = false;
 	} else {
 		onGround = true;
 		manPosition.yVelocity = 0;
-		manPosition.y = 0;
+		if (lrUp) {
+			manPosition.xVelocity = 0;			
+		}
+		manPosition.y = ground.offsetTop - man.offsetHeight + 1;
 	}
 }
 
 function moveMan() {
-	hitTest();
 	gravity();
 	manPosition.x += manPosition.xVelocity;
 	manPosition.y += manPosition.yVelocity;
 	man.style.left = `${manPosition.x}px`;
 	man.style.top = `${manPosition.y}px`;
+	hitTest();
 }
 
 function handleKeyDown(e) {
-	console.log(e);
 	// if (e.key === 'ArrowDown') {
 	// 	manPosition.y += speed;
 	// 	moveMan();
@@ -50,16 +53,20 @@ function handleKeyDown(e) {
 	// } else 
 	if (e.key === 'ArrowLeft') {
 		manPosition.xVelocity = -speed;
+		lrUp = false;
 	} else if (e.key === 'ArrowRight') {
 		manPosition.xVelocity = speed;
+		lrUp = false;
 	} else if (e.key === ' ') {
-		manPosition.yVelocity = -jumpSpeed;
-		console.log("l'espace")
+		if (onGround) {
+			manPosition.yVelocity = -jumpSpeed;
+		}
 	} 
 }
 function handleKeyUp(e) {
-	//console.log('up', e)
-	manPosition.xVelocity = 0;
+	if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+		lrUp = true;
+	}
 }
 
 setInterval(moveMan, 20);
