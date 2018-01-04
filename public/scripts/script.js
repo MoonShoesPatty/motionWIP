c50b5dec3b446565ecfe9d6b25662375e810f207
Pat Johnston <pattrashgarbage@gmail.com>
2018-01-04 18:26:32 -0500

    reconfigured using HTML5 canvas

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,75 +1,107 @@
-const ground = document.querySelector('.ground');
-const man = document.querySelector('.man');
+// Redraw function in canvas
+(function () {
+	const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+	window.requestAnimationFrame = requestAnimationFrame;
+})();
 
-const manPosition = {
-	x: 0,
-	y: 0,
+// Canvas selectors
+const canvas = document.getElementById('gameScreen');
+const ctx = canvas.getContext('2d');
+
+// Game size
+const gameWidth = window.innerWidth - 40;
+const gameHeight = window.innerHeight - 40;
+
+// Set up canvas
+canvas.width = gameWidth;
+canvas.height = gameHeight;
+canvas.style.top = '20px';
+canvas.style.left = '20px';
+
+// General game variables
+const playerSize = 30;
+const friction = 0.8;
+const gravity = 0.5;
+
+// Player object
+const player = {
+	x: gameWidth / 2.2,
+	y: gameHeight - playerSize,
+	width: playerSize,
+	height: playerSize,
+	speed: 5,
+	jumpSpeed: 10,
 	xVelocity: 0,
-	yVelocity: 0
-};
+	yVelocity: 0,
+	jumping: false
 
-const speed = 10;
-const gravitySpeed = 1;
-const jumpSpeed = 22;
-let onGround = false;
-let lrUp = true;
+	// User input
+};const keys = [];
 
-function gravity() {
-	if (!onGround) {
-		manPosition.yVelocity += gravitySpeed;
+// Redraw game
+function update() {
+	// Check keys
+	if (keys[38]) {
+		console.log('up');
 	}
-}
-
-function hitTest() {
-	//console.log(man.offsetTop + man.offsetHeight, ground.offsetTop);
-	if (man.offsetTop + man.offsetHeight <= ground.offsetTop) {
-		onGround = false;
-	} else {
-		onGround = true;
-		manPosition.yVelocity = 0;
-		if (lrUp) {
-			manPosition.xVelocity = 0;
-		}
-		manPosition.y = ground.offsetTop - man.offsetHeight + 1;
-	}
-}
-
-function moveMan() {
-	gravity();
-	manPosition.x += manPosition.xVelocity;
-	manPosition.y += manPosition.yVelocity;
-	man.style.left = `${manPosition.x}px`;
-	man.style.top = `${manPosition.y}px`;
-	hitTest();
-}
-
-function handleKeyDown(e) {
-	// if (e.key === 'ArrowDown') {
-	// 	manPosition.y += speed;
-	// 	moveMan();
-	// } else if (e.key === 'ArrowUp') {
-	// 	manPosition.y -= speed;
-	// 	moveMan();
-	// } else 
-	if (e.key === 'ArrowLeft') {
-		manPosition.xVelocity = -speed;
-		lrUp = false;
-	} else if (e.key === 'ArrowRight') {
-		manPosition.xVelocity = speed;
-		lrUp = false;
-	} else if (e.key === ' ') {
-		if (onGround) {
-			manPosition.yVelocity = -jumpSpeed;
+	if (keys[39]) {
+		if (player.xVelocity < player.speed) {
+			player.xVelocity++;
 		}
 	}
-}
-function handleKeyUp(e) {
-	if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
-		lrUp = true;
+	if (keys[37]) {
+		if (player.xVelocity > -player.speed) {
+			player.xVelocity--;
+		}
 	}
+	if (keys[38] || keys[32]) {
+		if (!player.jumping) {
+			player.jumping = true;
+			player.yVelocity = -player.jumpSpeed;
+		}
+	}
+	// Move player
+	if (!player.jumping) {
+		player.xVelocity *= friction;
+	}
+	player.yVelocity += gravity;
+	player.x += player.xVelocity;
+	player.y += player.yVelocity;
+	// Add horizontal bounds
+	if (player.x >= gameWidth - player.width) {
+		player.x = gameWidth - player.width;
+	} else if (player.x <= 0) {
+		player.x = 0;
+	}
+	// Add bottom boundary
+	if (player.y >= gameHeight - player.height) {
+		player.y = gameHeight - player.height;
+		player.jumping = false;
+	}
+	// Draw player
+	ctx.clearRect(0, 0, gameWidth, gameHeight);
+	ctx.fillStyle = '#12B4E9';
+	ctx.fillRect(player.x, player.y, player.width, player.height);
+	// Run the game!
+	requestAnimationFrame(update);
 }
 
-setInterval(moveMan, 20);
+// Handle keypresses
+function handleKeydown(e) {
+	keys[e.keyCode] = true;
+}
+function handleKeyup(e) {
+	keys[e.keyCode] = false;
+}
 
-document.addEventListener('keydown', handleKeyDown);
-document.addEventListener('keyup', handleKeyUp);
+// Key watchers
+document.addEventListener('keydown', handleKeydown);
+document.addEventListener('keyup', handleKeyup);
+
+// Init function
+function init() {
+	update();
+}
+
+// Document Ready
+document.addEventListener('DOMContentLoaded', init);
